201b7b3457290d275069eb0247e20c880568be2a
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2024-06-30 15:44:12 +0700

    update label

--- config/lang/en/label.php
+++ config/lang/en/label.php
@@ -1,1 +1,9 @@
-<?php return [];
+<?php 
+
+return [
+    'wa_devices' => 'Devices',
+    'wa_contacts' => 'Contacts',
+    'wa_templates' => 'Templates',
+    'wa_messages' => 'Messages',
+    'documentations' => 'Documentations'
+];
