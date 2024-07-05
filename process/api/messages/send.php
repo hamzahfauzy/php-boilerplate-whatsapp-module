12c431d4018ea8b7e3f9e4b728ab1e821fcfcec5
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2024-07-06 01:53:27 +0700

    update: send message by phone

--- process/api/messages/send.php
+++ process/api/messages/send.php
@@ -1,5 +1,6 @@
 <?php
 
+use Core\Database;
 use Core\Request;
 use Core\Response;
 
@@ -7,6 +8,24 @@ $responseCode = 405;
 $message = "Method is not allowed";
 if(Request::isMethod('POST'))
 {
+    if(isset($_POST['phone']))
+    {
+        $db = new Database;
+        $contact = $db->single('wa_contacts', ['phone' => $_POST['phone'], 'user_id' => auth()->id]);
+        if(empty($contact))
+        {
+            $contact = $db->insert('wa_contacts', [
+                'name' => $_POST['phone'],
+                'phone' => $_POST['phone'],
+                'user_id' => auth()->id,
+                'created_by' => auth()->id
+            ]);
+        }
+
+        $_POST['contact_id'] = $contact->id;
+        unset($_POST['phone']);
+    }
+
     $sendMessage = whatsappSendMessage($_POST, auth()->id);
     $message = $sendMessage['message'];
     $responseCode = $sendMessage['success'] ? 200 : 400;
