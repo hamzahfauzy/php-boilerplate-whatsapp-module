5b07704466acbb151a2384f7c062e0e787a7cb5f
root <root@idc30.m2server.my.id>
2024-11-28 21:05:16 -0500

    fix filter autoreply

--- hooks/create-fields-wa_replies.php
+++ hooks/create-fields-wa_replies.php
@@ -1,5 +1,12 @@
 <?php
 
+$isSuperAdmin = get_role(auth()->id)->role_id == 1;
+
+if(!$isSuperAdmin)
+{
+    $fields['device_id']['type'] .= '|user_id,'.auth()->id;
+}
+
 $fields['keyword']['attr'] = [
     'class' => 'form-control select2-search__field'
 ];
