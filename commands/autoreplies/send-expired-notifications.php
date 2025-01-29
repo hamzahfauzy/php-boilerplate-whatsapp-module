404e27c4edca77856b4ae5cf2870eb4955abc2ee
root <root@idc30.m2server.my.id>
2025-01-28 23:25:09 -0500

    add cron to set expired of campaign

--- commands/autoreplies/send-expired-notifications.php
+++ commands/autoreplies/send-expired-notifications.php
@@ -8,27 +8,35 @@ $db = new Database;
 // get expired reply session
 $expDate = date('Y-m-d H:i:s');
 Log::write("Expired notification start at : $expDate");
-$db->query = "SELECT wa_reply_sessions.*, wa_devices.user_id device_user_id FROM wa_reply_sessions JOIN wa_devices ON wa_devices.id = wa_reply_sessions.device_id WHERE wa_reply_sessions.`status` = 'ACTIVE' AND expired_at < NOW()";
+$db->query = "SELECT wa_reply_sessions.*, wa_devices.user_id device_user_id, wa_campaign_items.item_status campaign_status FROM wa_reply_sessions JOIN wa_devices ON wa_devices.id = wa_reply_sessions.device_id LEFT JOIN wa_campaign_items ON wa_campaign_items.session_id = wa_reply_sessions.id WHERE wa_reply_sessions.`status` = 'ACTIVE' AND expired_at < NOW()";
 $expiredSessions = $db->exec('all');
 
 foreach($expiredSessions as $session)
 {
-    // get setting
-    $setting = $db->single('wa_reply_settings',[
-        'user_id' => $session->device_user_id
-    ]);
-
-    $expiration_message = $setting->expiration_message;
-
-    // send expiration message to user
-    $data = [
-        'device_id'  => $session->device_id,
-        'contact_id' => [$session->contact_id],
-        'content'    => $expiration_message
-    ];
-
-    Log::write("Send Expired notification to : ". json_encode($data));
-    $sendMessage = whatsappSendMessage($data, $session->device_user_id);
+    if($session->campaign_status && $session->campaign_status == 'WAITING')
+    {
+        $db->query = "UPDATE wa_campaign_items SET `item_status` = 'EXPIRED', updated_at = NOW() WHERE session_id = $session->id";
+        $db->exec();
+    }
+    else
+    {
+        // get setting
+        $setting = $db->single('wa_reply_settings',[
+            'user_id' => $session->device_user_id
+        ]);
+    
+        $expiration_message = $setting->expiration_message;
+    
+        // send expiration message to user
+        $data = [
+            'device_id'  => $session->device_id,
+            'contact_id' => [$session->contact_id],
+            'content'    => $expiration_message
+        ];
+    
+        Log::write("Send Expired notification to : ". json_encode($data));
+        $sendMessage = whatsappSendMessage($data, $session->device_user_id);
+    }
 
     $db->query = "UPDATE wa_reply_sessions SET `status` = 'EXPIRED', updated_at = NOW() WHERE id = $session->id";
     $db->exec();
